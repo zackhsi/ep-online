3fd3c1d27718b42890a78291aa00a8836463cac6
Zack Hsi <zackhsi@gmail.com>
2020-11-30 09:46:10 +0800

    Pink Blossoms

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const { JSDOM } = require("jsdom");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  JSDOM.fromURL(
+    "https://shop.pinkblossomsbrewing.com/collections/featured-beers",
+    {}
+  ).then((dom) => {
+    const items = {};
+    dom.window.document.querySelectorAll(".grid__item").forEach((item) => {
+      const title = item.querySelector(".grid-view-item__title");
+      const price = item.querySelector(".price-item");
+      if (title != null && price != null) {
+        console.log(title.innerHTML.trim());
+        console.log(price.innerHTML.trim());
+        items[title.innerHTML.trim()] = price.innerHTML.trim();
+      }
+    });
+    const itemsString = JSON.parse(JSON.stringify(items));
+    console.log(itemsString);
+    core.setOutput("items", itemsString);
+  });
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
